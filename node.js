2cb7bfe6eabf5d427a9ee5539678114723e19e8e
Tyler Long <tyler4long@gmail.com>
2019-11-07 19:56:37 -0800

    Fix a typo

--- node.js
+++ node.js
@@ -4,14 +4,14 @@ const fs = require('fs')
 
 const SIP = {
   set: (name, value) => {
-    fs.writeFileSync(path.join(os.tempdir(), name), value)
+    fs.writeFileSync(path.join(os.tmpdir(), name), value)
   },
   get: name => {
-    return JSON.parse(fs.readFileSync(path.join(os.tempdir(), name), { encoding: 'utf-8' }))
+    return JSON.parse(fs.readFileSync(path.join(os.tmpdir(), name), { encoding: 'utf-8' }))
   },
   remove: name => {
-    if (fs.existsSync(path.join(os.tempdir(), name))) {
-      fs.unlinkSync(path.join(os.tempdir(), name))
+    if (fs.existsSync(path.join(os.tmpdir(), name))) {
+      fs.unlinkSync(path.join(os.tmpdir(), name))
     }
   }
 }
